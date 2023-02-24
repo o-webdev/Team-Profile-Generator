7a11c92f4d93155f5df55ce9f3e815d5cbc77684
Omar-ahm <irycevier@gmail.com>
2023-02-24 20:05:13 +0000

    prompt for manager and manager questions updated on index.js

--- index.js
+++ index.js
@@ -13,18 +13,49 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// Array to hold team members
+const teamMembers = [];
 // Starter pseudocode provided in class 
 
+// Prompts for managers information 
+const promptForManager =  () => {
 inquirer.prompt([{
     //manager questions
-}]).then(response => {
+    type: 'input',
+    name: 'name',
+    message: 'What is the name of the team Manager?'
+},
+{
+    type: 'input',
+    name: 'id',
+    message: 'What is the employee ID of the team Manager?'
+},
+{
+    type: 'input',
+    name: 'email',
+    message:'What is the email address of the team Manager?'
+},
+{
+    type: 'input',
+    name: 'officeNumberl',
+    message:'What is the office number of the team Manager?'  
+}
+]).then(response => {
     // populate manager info
+    const manager = new Manager(response.naem, response.id, response.email, response.officeNumber);
+    teamMembers.push(manager);
     // promptForNexEmployee ()
-})
+    promptForNextEmployee();
+});
+}
 
 const promptForNextEmployee = () => {
     inquirer.prompt([{
         // choice of 3
+        type: 'list',
+        name: 'role',
+        message: 'Which type of team memeber would you like to add?',
+        choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members']
     }]).then(response => {
         // if engineer
         //    promptForEngineer
